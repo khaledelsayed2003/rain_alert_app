3aca8d6b9a6acc562ccabc86552a8ae3df608b3d
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-03 20:58:41 +0300

    Set up secure API key management using python-dotenv and create requirements file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-# rain alert app
+import requests
+from dotenv import load_dotenv
+import os
+
+
+# Load the .env file from the config folder
+load_dotenv("rain_alert_app/config/.env")
+
+MY_OWN_API_KEY = os.getenv("WEATHER_API_KEY")
