21c7251b0201b23ed30c0dbb6e4f13c1796adb69
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-04 15:51:33 +0300

    Add rain detection logic and single umbrella notification for forecast intervals

--- src/main.py
+++ src/main.py
@@ -8,17 +8,30 @@ load_dotenv("rain_alert_app/config/.env")
 MY_OWN_API_KEY = os.getenv("WEATHER_API_KEY")
 
 # This latitude and longitude is for Istanbul City.
-MY_LAT = 41.013000
-MY_LONG = 28.974800 
+MY_LAT = 40.991309
+MY_LONG = 28.771965
+
+# Number of 3-hour forecast intervals to retrieve (4 intervals ≈ 12 hours)
+forecast_count = 4
     
 parameters = {
     "lat": MY_LAT, 
     "lon": MY_LONG,
     "appid": MY_OWN_API_KEY, 
-    "cnt": 4
+    "cnt": forecast_count
 }
 
 forecast_3h_response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params= parameters)
 forecast_3h_response.raise_for_status()
 weather_data = forecast_3h_response.json()
-print(weather_data)
+
+
+# Check each 3-hour interval, but notify only once if any forecast predicts rain
+for iteration in range(forecast_count):
+    weather_id = weather_data["list"][iteration]["weather"][0]["id"]
+    if int(weather_id) < 700: # Weather codes <700 = rain/snow/drizzle/thunderstorm
+        print("Bring an umbrella")
+        break # Stop checking further forecasts to avoid repeated notifications
+    
+
+        
