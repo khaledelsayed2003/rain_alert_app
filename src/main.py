b210bca7456df8e1ac8d5f3bd22f3227d8765da7
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-04 17:26:05 +0300

    Integrate Twilio SMS alert and send message when rain is detected

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import requests
 from dotenv import load_dotenv
 import os
+from twilio.rest import Client
 
 
 # Load the .env file from the config folder
 load_dotenv("rain_alert_app/config/.env")
 MY_OWN_API_KEY = os.getenv("WEATHER_API_KEY")
+
+# Load your own Account SID and Auth Token from the .env file.
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+client = Client(account_sid, auth_token)
+
 
 # This latitude and longitude is for Istanbul City.
 MY_LAT = 40.991309
@@ -30,7 +38,11 @@ weather_data = forecast_3h_response.json()
 for iteration in range(forecast_count):
     weather_id = weather_data["list"][iteration]["weather"][0]["id"]
     if int(weather_id) < 700: # Weather codes <700 = rain/snow/drizzle/thunderstorm
-        print("Bring an umbrella")
+        message = client.messages.create(
+            body="☔ Rain is expected soon. Please bring an umbrella, Mr. Khaled.",
+            from_="+16105107634",
+            to="+905010823454",
+        )
         break # Stop checking further forecasts to avoid repeated notifications
     
 
