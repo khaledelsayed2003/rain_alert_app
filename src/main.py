2d2c1235ea31f5683fbc936ce9af7814eb40cb90
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-04 14:10:56 +0300

    Add 3-hour weather forecast API request with OpenWeather parameters

--- src/main.py
+++ src/main.py
@@ -5,5 +5,19 @@ import os
 
 # Load the .env file from the config folder
 load_dotenv("rain_alert_app/config/.env")
-
 MY_OWN_API_KEY = os.getenv("WEATHER_API_KEY")
+
+# This latitude and longitude is for Istanbul City.
+MY_LAT = 41.013000
+MY_LONG = 28.974800 
+    
+parameters = {
+    "lat": MY_LAT, 
+    "lon": MY_LONG,
+    "appid": MY_OWN_API_KEY
+}
+
+forecast_3h_response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params= parameters)
+forecast_3h_response.raise_for_status()
+data = forecast_3h_response.json()
+print(data)
